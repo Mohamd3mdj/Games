fcb8ecaa9dce83e29ecfb464d3c8b1ca15d8bfb3
mohamed1mekawy <95969402+Mohamd3mdj@users.noreply.github.com>
2024-03-02 21:50:09 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,89 @@
+//  // //  //  //
+// WHILE LOOP //
+// // // //  //
+
+/*let logd = false;
+let username;
+let password;
+
+while(!logd){
+    username = window.prompt('enter your username')
+    password = window.prompt('enter your password')
+
+if (username === "Mohamed44mekawy" || password ==="Moka1997733#"){
+    logd=true;
+    console.log ("You are loggedin!!");
+}
+else{
+    console.log("invalid , please try again")
+}
+}
+*/
+
+//  // //  // //
+// FOR  LOOP //
+// // // //  //
+
+//for (let i=377; i>0;i++){
+//console.log(i+"a"); }
+//console.log("GHGFDSGFS")
+/*
+for (let i = 1 ;i<=20 ; i++ ){
+    
+    if(i===13){
+        continue;
+    }
+    else{
+        console.log(i); 
+    } ?///*/ 
+
+// Number gussing game//////////////////////
+const min =1;
+const max=100;
+const answer =Math.floor(Math.random()*(max-min+1))+ min;
+
+let attempts =0;
+let guess;
+let running =true;
+
+while(running){
+    guess = window.prompt(`Guess a number between ${min} - ${max}`);
+    guess = Number(guess);
+
+    if (isNaN(guess)){
+        window.alert("please try again:");
+    }
+    else if (guess < min || guess > max){
+        window.alert("please enter a valid number:");
+    }
+    else{
+    attempts++;
+    if(guess<answer){
+        window.alert("Too Low !Try again!");
+    }
+    else if (guess>answer){
+     window.alert("TOO High !!Try again!!");
+    }
+     else{
+        window.alert(`WWOOOOOOO THATS CORECT  yaaaaaa faaaaaaarohtiiiiiiiiiiiiii!!! was ${answer} it ${attempts} attempets بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك بحبك شطوووووووووووووووووووووووووووووووووووووووورة عااااااااااااااااااااااااااااااااااااااااش` );
+        running=false; 
+    }
+    }
+}
+
+//  // //  //  //
+// FuN C T ION//
+// // // //  //]]
+
+/*
+function happy (user , agew){
+ console.log(`sddssd: ${user}`) ;
+ console.log (`jhgyfyj:${agew}`) ;  
+}
+happy (22,22);
+happy ("sddsd" ,"fddf");*/
+
+//function mamo(x,y) {
+//    return x+y;
+//}
+//console.log(mamo(3,520));
